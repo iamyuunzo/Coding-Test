f07c8a011fb12105e1778408fe203cab59f690bf
조윤주 <155442976+iamyuunzo@users.noreply.github.com>
2025-07-07 21:30:52 +0900

    [Bronze V] Title: Rook, Time: 96 ms, Memory: 13948 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("  ___  ___  ___\n");
+        System.out.print("  | |__| |__| |\n");
+        System.out.print("  |           |\n");
+        System.out.print("   \\_________/\n");
+        System.out.print("    \\_______/\n");
+        System.out.print("     |     |\n");
+        System.out.print("     |     |\n");
+        System.out.print("     |     |\n");
+        System.out.print("     |     |\n");
+        System.out.print("     |_____|\n");
+        System.out.print("  __/       \\__\n");
+        System.out.print(" /             \\\n");
+        System.out.print("/_______________\\\n");
+    }
+}
