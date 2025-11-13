33536974458a1ea0d6a23d90c986878d687f5a08
조윤주 <155442976+iamyuunzo@users.noreply.github.com>
2025-11-13 23:26:38 +0900

    [D1] Title: 대각선 출력하기, Time: 75 ms, Memory: 24,448 KB -BaekjoonHub

---
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.java
@@ -0,0 +1,13 @@
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		System.out.println("#++++");
+        System.out.println("+#+++");
+        System.out.println("++#++");
+        System.out.println("+++#+");
+        System.out.println("++++#");
+	}
+}
