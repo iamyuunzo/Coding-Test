e710341482f35fbfe36c3630b4c0f7eaf40f4785
조윤주 <155442976+iamyuunzo@users.noreply.github.com>
2025-11-15 17:41:12 +0900

    [D1] Title: 더블더블, Time: 92 ms, Memory: 26,752 KB -BaekjoonHub

---
+++ SWEA/D1/2019. 더블더블/더블더블.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+        
+        for (int i = 0; i <= N; i++) {
+            System.out.print((int)Math.pow(2, i) + " ");
+        }
+	}
+}
