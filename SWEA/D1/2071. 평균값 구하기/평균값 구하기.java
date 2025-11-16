740663bab052483000330c8db4aa17e2af2e8c29
조윤주 <155442976+iamyuunzo@users.noreply.github.com>
2025-11-16 19:04:14 +0900

    [D1] Title: 평균값 구하기, Time: 93 ms, Memory: 26,624 KB -BaekjoonHub

---
+++ SWEA/D1/2071. 평균값 구하기/평균값 구하기.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++) {
+            double sum = 0;
+
+			for (int i = 1; i <= 10; i++) {
+            	int N = sc.nextInt();
+                sum += N;
+            }
+            System.out.println("#" + test_case + " " + Math.round(sum/10));
+		}
+	}
+}
